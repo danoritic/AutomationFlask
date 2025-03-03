13e62a67786a3421be6a501c13ef6e7023863c16
Olasiyan Daniel <danoritic@gmail.com>
2025-03-04 00:52:32 +0100

    assasasa

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app import create_app
 
 # This is required for Vercel serverless deployment
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=5001) 
+    create_app().run(host="0.0.0.0", port=5001)
